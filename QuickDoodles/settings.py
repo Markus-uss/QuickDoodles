82ba574b1a54229d8551005d5a93e7a43398d833
Vegan Toast <markuslu.bc@gmail.com>
2026-08-12 12:30:12 -0700

    prepping for render

--- QuickDoodles/settings.py
+++ QuickDoodles/settings.py
@@ -130,11 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
